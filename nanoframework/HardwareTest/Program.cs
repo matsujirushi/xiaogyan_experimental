7fd1c4f0ec3effe3213bf669f8ef012b0ec1adf1
MATSUOKA Takashi <matsujirushi@live.jp>
2023-07-11 11:19:10 +0900

    Xiaogyanクラス追加

--- nanoframework/HardwareTest/Program.cs
+++ nanoframework/HardwareTest/Program.cs
@@ -1,35 +1,24 @@
-using System.Device.Gpio;
+using System;
 using System.Threading;
 
 namespace HardwareTest
 {
     public class Program
     {
-        private const int _LedPin = 21;
-        private const int _ButtonAPin = 9;
-        private const int _ButtonBPin = 20;
-
         public static void Main()
         {
-            using var gpio = new GpioController();
-
-            using var ledRaw = gpio.OpenPin(_LedPin, PinMode.Output);
-            var led = new InvertGpioPin(ledRaw);
-
-            using var buttonARaw = gpio.OpenPin(_ButtonAPin, PinMode.InputPullUp);
-            using var buttonBRaw = gpio.OpenPin(_ButtonBPin, PinMode.InputPullUp);
-            var buttonA = new InvertGpioPin(buttonARaw);
-            var buttonB = new InvertGpioPin(buttonBRaw);
+            using var _Xiaogyan = new Xiaogyan();
+            _Xiaogyan.begin();
 
             while (true)
             {
-                if (buttonA.Read() != 0)
+                if (_Xiaogyan.ButtonA.Read() != 0)
                 {
-                    led.Write(1);
+                    _Xiaogyan.Led.Write(1);
                 }
                 else
                 {
-                    led.Write(0);
+                    _Xiaogyan.Led.Write(0);
                 }
 
                 Thread.Sleep(100);
---
+++ nanoframework/HardwareTest/Xiaogyan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace HardwareTest
+{
+    internal class Xiaogyan : IDisposable
+    {
+        public InvertGpioPin Led;
+        public InvertGpioPin ButtonA;
+        public InvertGpioPin ButtonB;
+
+        public void begin()
+        {
+            var gpio = new GpioController();
+
+            _LedRaw = gpio.OpenPin(_LedPin, PinMode.Output);
+            _ButtonARaw = gpio.OpenPin(_ButtonAPin, PinMode.InputPullUp);
+            _ButtonBRaw = gpio.OpenPin(_ButtonBPin, PinMode.InputPullUp);
+
+            Led = new InvertGpioPin(_LedRaw);
+            ButtonA = new InvertGpioPin(_ButtonARaw);
+            ButtonB = new InvertGpioPin(_ButtonBRaw);
+        }
+
+        public void Dispose()
+        {
+            _LedRaw?.Dispose();
+            _LedRaw = null;
+            _ButtonARaw?.Dispose();
+            _ButtonARaw = null;
+            _ButtonBRaw?.Dispose();
+            _ButtonBRaw = null;
+        }
+
+        private const int _LedPin = 21;
+        private const int _ButtonAPin = 9;
+        private const int _ButtonBPin = 20;
+
+        private GpioPin _LedRaw;
+        private GpioPin _ButtonARaw;
+        private GpioPin _ButtonBRaw;
+
+    }
+}
